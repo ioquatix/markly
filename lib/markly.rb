a0673093bf7423c324ab422af888d16d59801336
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2020-05-24 00:50:36 +1200

    WIP

--- lib/markly.rb
+++ lib/markly.rb
@@ -1,17 +1,33 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+# The compiled library.
 require 'markly/markly'
-require 'markly/config'
+
+require 'markly/flags'
 require 'markly/node'
 require 'markly/renderer'
 require 'markly/renderer/html_renderer'
 require 'markly/version'
 
-begin
-  require 'awesome_print'
-rescue LoadError; end # rubocop:disable Lint/SuppressedException
 module Markly
+  # Public: Parses a Markdown string into a `document` node.
+  #
+  # string - {String} to be parsed
+  # option - A {Symbol} or {Array of Symbol}s indicating the parse options
+  # extensions - An {Array of Symbol}s indicating the extensions to use
+  #
+  # Returns the `parser` node.
+  def self.parse(text, flags: DEFAULT, extensions: nil)
+    parser = Parser.new(flags)
+    
+    extensions&.each do |extension|
+      parser.enable(extension)
+    end
+    
+    return parser.parse(text.encode(Encoding::UTF_8))
+  end
+  
   # Public:  Parses a Markdown string into an HTML string.
   #
   # text - A {String} of text
@@ -19,27 +35,9 @@ module Markly
   # extensions - An {Array of Symbol}s indicating the extensions to use
   #
   # Returns a {String} of converted HTML.
-  def self.render_html(text, options = :DEFAULT, extensions = [])
-    raise TypeError, "text must be a String; got a #{text.class}!" unless text.is_a?(String)
-
-    opts = Config.process_options(options, :render)
-    text = text.encode('UTF-8')
-    html = Node.markdown_to_html(text, opts, extensions)
-    html.force_encoding('UTF-8')
-  end
-
-  # Public: Parses a Markdown string into a `document` node.
-  #
-  # string - {String} to be parsed
-  # option - A {Symbol} or {Array of Symbol}s indicating the parse options
-  # extensions - An {Array of Symbol}s indicating the extensions to use
-  #
-  # Returns the `document` node.
-  def self.parse(text, options = :DEFAULT, extensions = [])
-    raise TypeError, "text must be a String; got a #{text.class}!" unless text.is_a?(String)
-
-    opts = Config.process_options(options, :parse)
-    text = text.encode('UTF-8')
-    Node.parse_document(text, text.bytesize, opts, extensions)
+  def self.render_html(text, flags: DEFAULT, extensions: nil)
+    root = self.parse(text, flags: flags, extensions: extensions)
+    
+    return root.to_html
   end
 end
---
+++ lib/markly/flags.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Markly
+	DEFAULT = 0
+	VALIDATE_UTF8 = 1 << 9
+	SMART = 1 << 10
+	LIBERAL_HTML_TAG = 1 << 12
+	FOOTNOTES = 1 << 13
+	STRIKETHROUGH_DOUBLE_TILDE = 1 << 14
+	UNSAFE = 1 << 17
+
+	# Renderer Flags
+	SOURCE_POSTIONS = 1 << 1
+	HARD_BREAKS = 1 << 2
+	NO_BREAKS = 1 << 4
+	GITHUB_PRE_LANG = 1 << 11
+	TABLE_PREFER_STYLE_ATTRIBUTES = 1 << 15
+	FULL_INFO_STRING = 1 << 16
+end
