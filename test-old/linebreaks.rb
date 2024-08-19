50fcac68d1d38fb79aa3511bd0a7521daef0b944
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2024-08-19 17:16:32 +1200

    Apply RuboCop to old tests.

--- test-old/linebreaks.rb
+++ test-old/linebreaks.rb
@@ -8,13 +8,13 @@
 require 'test_helper'
 
 class TestLinebreaks < Minitest::Test
-  def test_hardbreak_no_spaces
-    doc = Markly.parse("foo\nbaz")
-    assert_equal "<p>foo<br />\nbaz</p>\n", doc.to_html(flags: Markly::HARD_BREAKS)
-  end
+	def test_hardbreak_no_spaces
+		doc = Markly.parse("foo\nbaz")
+		assert_equal "<p>foo<br />\nbaz</p>\n", doc.to_html(flags: Markly::HARD_BREAKS)
+	end
 
-  def test_hardbreak_with_spaces
-    doc = Markly.parse("foo  \nbaz")
-    assert_equal "<p>foo<br />\nbaz</p>\n", doc.to_html(flags: Markly::HARD_BREAKS)
-  end
+	def test_hardbreak_with_spaces
+		doc = Markly.parse("foo  \nbaz")
+		assert_equal "<p>foo<br />\nbaz</p>\n", doc.to_html(flags: Markly::HARD_BREAKS)
+	end
 end
